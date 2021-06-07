cb597aae6824b94c04bd2f84a9f9747f6af86a33
ytmk0116 <ytmk0116@gmail.com>
2021-06-07 20:26:33 +0000

    CRUD機能実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,12 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
+    #データベースに保存されている全てのタスクデータを取得
   end
 
   def show
+    @task = Task.find(params[:id])
+    #findメソッドでレコードをデータベースから検索
   end
 
   def new
@@ -17,8 +21,21 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params) #update!メソッドでtaskパラメーターの代入とデータベースへの保存の両方を担っている
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
+  end
+  
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
+  end
+  
   private
 
   def task_params
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Success
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = 'Asia/Tokyo'
+    # 世界時間から日本時間に変更
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
